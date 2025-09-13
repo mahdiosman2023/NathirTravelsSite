f5524fc44aa4485903f0ba97dd059a700274cc57
Mohamed Ibrahim <abdulhakimm280@gmail.com>
2025-09-13 16:45:37 +0300

    Fix nodemailer.createTransport bug in API function

--- api/send.js
+++ api/send.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 // Gmail SMTP configuration with proper authentication
 const createMailTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
